ad044c67af3710877681586bc8459f6437de8592
Hamin Kim <dev.hamin.kim@gmail.com>
2024-11-25 10:43:16 +0900

    Initial Commit

---
+++ Kiosk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kiosk
+//
+//  Created by 김하민 on 11/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
