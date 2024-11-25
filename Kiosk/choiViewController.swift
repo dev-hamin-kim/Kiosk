2486d5d4a03120c7f2de2ac1a0237a955ecf49e1
Choi <chltjdw@naver.com>
2024-11-25 12:01:24 +0900

    [new] created CSJViewController

--- Kiosk/choiViewController.swift
+++ Kiosk/choiViewController.swift
@@ -5,25 +5,3 @@
 //  Created by t2023-m0072 on 11/25/24.
 //
 
-import UIKit
-
-class choiViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //Choi
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
